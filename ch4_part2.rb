435c9e03704e7ebdca3726d40ecc6fded128571c
Robert Laws <roblaws@me.com>
2017-10-15 20:02:29 +0300

    finish chapter 4 - initialize and class methods

--- ch4_part2.rb
+++ ch4_part2.rb
@@ -3,6 +3,10 @@
 class Employee
   attr_reader :name
 
+  def initialize(name = "Anonymous")
+    @name = name
+  end
+
   def name=(name)
     if name == ""
       raise "name cannot be blank"
@@ -19,7 +23,7 @@ class SalariedEmployee < Employee
   attr_reader :salary
 
   def initialize(name = "Anonymous", salary = 0.0)
-    self.name = name
+    super(name)
     self.salary = salary
   end
 
@@ -38,5 +42,46 @@ class SalariedEmployee < Employee
   end
 end
 
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "a hourly wage of #{hourly_wage} is not valid."
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "hours per week of #{hours_per_week} is not valid."
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def self.make_cashier(name)
+    HourlyEmployee.new(name, 12.5, 30)
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay for this period: #{formatted_pay}."
+  end
+end
+
 bob = SalariedEmployee.new("Bob", 50000)
 bob.print_pay_stub
+
+hal = HourlyEmployee.new("Hal", 23.5, 40)
+hal.print_pay_stub
+
+kal = HourlyEmployee.make_cashier("Kal")
+kal.print_pay_stub
