e11727e14a31a5f027cdcd1fa9f974277c11e879
Robert Laws <roblaws@me.com>
2017-10-15 15:58:36 +0300

    add salaried employee code

--- ch4_part2.rb
+++ ch4_part2.rb
@@ -1,2 +1,42 @@
 # Using initialize with inheritance
 
+class Employee
+  attr_reader :name
+
+  def name=(name)
+    if name == ""
+      raise "name cannot be blank"
+    end
+    @name = name
+  end
+
+  def print_name
+    puts "Name: #{name}"
+  end
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def initialize(name = "Anonymous", salary = 0.0)
+    self.name = name
+    self.salary = salary
+  end
+
+  def salary=(salary)
+    if salary < 0
+      raise "a salary of #{salary} is not valid"
+    end
+    @salary = salary
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay this period: #{formatted_pay}."
+  end
+end
+
+bob = SalariedEmployee.new("Bob", 50000)
+bob.print_pay_stub
